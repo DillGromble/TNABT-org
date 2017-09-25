333735bdd89568d60f8d1c4d8c1a8dee27a30272
Josh White <whitejosh42@gmail.com>
2017-09-25 11:39:36 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#Tennessee Association of Biology Teachers
+
+This is the website for the Tennessee Association of Biology Teachers.
+
+## Authors
+
+* **Josh White** - *Sole Contributor* - [DillGromble](https://github.com/DillGromble)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>TNABT</h2>
+        </div>
+        <p className="App-intro">
+          The Tennessee Association of Biology Teachers
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
