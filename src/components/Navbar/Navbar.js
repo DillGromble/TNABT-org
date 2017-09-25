f472a677279188d064f17c7be66b4e806b1bda92
Josh White <whitejosh42@gmail.com>
2017-09-25 17:18:35 -0500

    feat(navbar): add logo and queries for responsive layout

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,6 +4,7 @@ import './Navbar.css'
 
 const Navbar = () => (
   <nav className="nav-container">
+    <img className="logo-nav" src="/assets/img/TNABT_logo.png" alt="TNABT Logo" />
     <ul className="main-nav">
       <li>Login</li>
       <li>About Us</li>
@@ -11,6 +12,7 @@ const Navbar = () => (
       <li>Awards and Service</li>
       <li>Resources</li>
     </ul>
+    <i className="ion-navicon-round mobile-nav"></i>
   </nav>
 )
 
---
+++ src/views/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './Home.css'
+import './img/home-hero.jpg'
+
+const Home = () => (
+  <div className="home-container">
+    <header>
+      <div className="hero-text-box">
+        <h1>TNABT</h1>
+      </div>
+    </header>
+  </div>
+)
+
+export default Home
