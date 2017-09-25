5b7722d7db562f328cb7011e68bb6ab308f8da47
Josh White <whitejosh42@gmail.com>
2017-09-25 14:05:25 -0500

    feat(navbar): add basic navbar content and styling

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './Navbar.css'
+
+const Navbar = () => (
+  <nav className="nav-container">
+    <ul className="main-nav">
+      <li>Login</li>
+      <li>About Us</li>
+      <li>Membership</li>
+      <li>Awards and Service</li>
+      <li>Resources</li>
+    </ul>
+  </nav>
+)
+
+export default Navbar
