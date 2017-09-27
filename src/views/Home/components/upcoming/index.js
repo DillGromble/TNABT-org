bc1b5b0113c1ed95fd1b3389e83ad5652934019d
Josh White <whitejosh42@gmail.com>
2017-09-27 11:38:53 -0500

    feat(queries): add tablet queries for upcoming events

--- src/views/Home/components/upcoming/index.js
+++ src/views/Home/components/upcoming/index.js
@@ -11,9 +11,9 @@ const Upcoming = () => (
   <section className="section-upcoming">
     <h2>Upcoming Events</h2>
     <div className="upcoming-container">
-      <img src={west} className="upcoming-west region" alt="West TN Events" />
-      <img src={mid} className="upcoming-mid region" alt="Middle TN Events" />
-      <img src={east} className="upcoming-east region" alt="East TN Events" />
+      <img src={ west } className="upcoming-west region" alt="West TN Events" />
+      <img src={ mid } className="upcoming-mid region" alt="Middle TN Events" />
+      <img src={ east } className="upcoming-east region" alt="East TN Events" />
       </div>
       <p>Click a region to see their events!</p>
   </section>
