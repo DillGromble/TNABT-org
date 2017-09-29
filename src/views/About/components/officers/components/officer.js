a57a8b8b2c02f87d2d4787b4dd05edb06c2d40fe
Josh White <whitejosh42@gmail.com>
2017-09-29 17:06:03 -0500

    feat(officer): add single officer component and styling

--- src/views/About/components/officers/components/officer.js
+++ src/views/About/components/officers/components/officer.js
@@ -3,14 +3,25 @@ import React from 'react'
 import './officer.css'
 
 
-const Officer = () => (
+const Officer = ({ name, office, school, classes, phone, email, image }) => (
   <div className="officer-component">
     <img
       className="officer-headshot"
-      src="https://s3.amazonaws.com/tnabt-content/Officers/Treasurer/Bob.jpg"
-      alt="TNABT Treasurer"
+      src={image}
+      alt={`TNABT ${office}`}
     />
+    <hr width="1" size="320" />
+    <div className="officer-info">
+      <h3 className="officer-name">{name}</h3>
+      <h4>{office}</h4>
+      <p>{school}</p>
+      <p>{classes}</p>
+      <p>{phone}</p>
+      <p>{email}</p>
+    </div>
   </div>
 )
 
+
 export default Officer
+
--- src/views/About/components/officers/index.js
+++ src/views/About/components/officers/index.js
@@ -1,13 +1,25 @@
 import React from 'react'
 
+import './officers.css'
+
 import Officer from './components/officer'
+
+const officerTest = {
+  name: 'Robert Pruitt',
+  office: 'Secretary',
+  school: 'Montgomery Bell Academy, Nashville',
+  classes: 'Biology I and AP Biology',
+  phone: '(615) 298-5514  ext: 326',
+  email: 'robert.pruitt@montgomerybell.edu',
+  image: 'https://s3.amazonaws.com/tnabt-content/Officers/Treasurer/Bob.jpg'
+}
 
 
 const Officers = () => (
   <section className="section-officers">
     <div className="row">
       <h2>Officers</h2>
-      <Officer />
+      <Officer {...officerTest} />
     </div>
   </section>
 )
