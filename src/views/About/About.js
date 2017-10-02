95e72ad884cafab4f84d3256eac35e841a5475a3
Josh White <whitejosh42@gmail.com>
2017-10-02 12:37:56 -0500

    feat(carousel): add responsive carousel and dummy pics

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 import Header from './components/header'
 import Officers from './components/officers'
-
+import PhotoCarousel from './components/carousel'
 
 const About = () => (
   <div>
     <Header />
+    <PhotoCarousel />
     <Officers />
   </div>
 )
---
+++ src/views/About/components/carousel/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
+
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import './carousel.css'
+
+import pic1 from './img/1.jpg'
+import pic2 from './img/2.jpg'
+import pic3 from './img/3.jpg'
+import pic4 from './img/4.jpg'
+
+
+const PhotoCarousel = () => (
+  <section className="row section-carousel">
+    <h2>Past Events</h2>
+    <div className="photo-carousel">
+      <Carousel infiniteLoop={true} autoPlay={true} interval={5000}>
+        <div>
+          <img src={pic1} alt="TNABT Event" />
+        </div>
+        <div>
+          <img src={pic2} alt="TNABT Event" />
+        </div>
+        <div>
+          <img src={pic3} alt="TNABT Event" />
+        </div>
+        <div>
+          <img src={pic4} alt="TNABT Event" />
+        </div>
+      </Carousel>
+    </div>
+  </section>
+)
+
+export default PhotoCarousel
