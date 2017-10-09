e7bb7107267d2a4374d1b9e16ebb759d0aef4cf0
Josh White <whitejosh42@gmail.com>
2017-10-09 18:53:57 -0500

    feat(tipOfMonth): add row of divs for image and prompt

--- src/views/Resources/Resources.js
+++ src/views/Resources/Resources.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 import './resources.css'
-
+import tipLogo from './img/tipLogo.png'
 import Header from './components/header'
+
+
+import TipOverview from './components/tip-overview'
+import FloatPrompt from './components/float-prompt'
 
 
 const Resources = () => (
@@ -15,6 +19,26 @@ const Resources = () => (
         The Tip of the Month is the place to show off you great interactive, dry
         lab, etc, that has been effective in your life science classes.
       </p>
+      <TipOverview />
+
+      <div className="row flex">
+        <FloatPrompt
+          message={'Want to see our tips from previous months?'}
+          btnText={'Login'}
+          onClick={ () => alert('login!') }
+        />
+        <img
+          className="boxes"
+          src={tipLogo}
+          alt="Tip of the Month"
+        />
+        <FloatPrompt
+          message={'Got a great idea that has worked for your classes?'}
+          btnText={'Share it!'}
+          onClick={ () => alert('contact!') }
+        />
+      </div>
+
     </section>
 
   </div>
