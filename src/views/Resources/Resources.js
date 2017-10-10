eec69561807e683f54965dd1b1a8df0f586e0681
Josh White <whitejosh42@gmail.com>
2017-10-10 13:09:00 -0500

    feat(gear): add gear section

--- src/views/Resources/Resources.js
+++ src/views/Resources/Resources.js
@@ -11,6 +11,8 @@ import TSTAlogo from './img/TSTA_logo.png'
 import ASBlogo from './img/ASB_logo.png'
 import LandsEndLogo from './img/lands_end_logo.png'
 
+
+
 const Resources = () => (
   <div>
     <Header />
@@ -48,6 +50,23 @@ const Resources = () => (
       </div>
     </section>
 
+    <section className="section-resource-gear">
+      <h2>NABT Gear</h2>
+      <div className="lands-end-container">
+        <p className="copy">
+          Lands' End offers clothing with the NABT logo.  You can shop for clothing
+          with the option to add the NABT logo using the link provided:
+        </p>
+        <a href="https://business.landsend.com/store/nabt/">
+          <div className="resource-link lands-end">
+            <img
+              src={LandsEndLogo}
+              alt="Link to NABT Lands End Gear"
+            />
+          </div>
+        </a>
+      </div>
+    </section>
   </div>
 )
 
