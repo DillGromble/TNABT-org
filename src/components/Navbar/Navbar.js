ac59bad835a367b6dce837c336f206c33cc2c687
Josh White <whitejosh42@gmail.com>
2017-10-11 13:56:15 -0500

    feat(nav): add mobile nav functionality

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,34 +1,72 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router'
 
 import './Navbar.css'
 
 
-const Navbar = () => (
-  <nav className="nav-container">
+class Navbar extends Component {
 
-    <Link to="/home">
-      <img className="logo-nav" src="assets/img/TNABT_logo.png" alt="TNABT Logo" />
-    </Link>
+  constructor() {
+    super()
+    this.state = ({
+      hamburgerOpen: false,
+      mobileLinkState: '',
+      hamburgerIcon: 'ion-navicon-round'
+    })
+
+    this.toggleDropdown = this.toggleDropdown.bind(this)
+    this.handleMobileClick = this.handleMobileClick.bind(this)
+  }
 
 
-    <ul className="main-nav">
-      <li>
-        <Link to="/about">About Us</Link>
-      </li>
-      <li>
-        <Link to="/membership">Membership</Link>
-      </li>
-      <li>
-        <Link to="/awards-service">Awards and Service</Link>
-      </li>
-      <li>
-        <Link to="/resources">Resources</Link>
-      </li>
-      <li>Login</li>
-    </ul>
-    <i className="ion-navicon-round mobile-nav"></i>
-  </nav>
-)
+  toggleDropdown() {
+    const hamburgerOpen = !this.state.hamburgerOpen
+    const [ hamburgerIcon, mobileLinkState ] = hamburgerOpen
+      ? ['ion-close-round close-icon', 'mobile-nav-open']
+      : ['ion-navicon-round', '']
+    this.setState({ hamburgerOpen, mobileLinkState, hamburgerIcon })
+  }
+
+
+  handleMobileClick() {
+    if (this.state.hamburgerOpen) this.toggleDropdown()
+  }
+
+
+  render() {
+    const { hamburgerIcon, mobileLinkState } = this.state
+    const { handleMobileClick, toggleDropdown } = this
+
+    return (
+      <nav className="nav-container">
+        <Link to="/home" onClick={handleMobileClick}>
+          <img
+            className="logo-nav"
+            src="assets/img/TNABT_logo.png"
+            alt="TNABT Logo"
+          />
+        </Link>
+
+        <i className={`${hamburgerIcon} mobile-nav`} onClick={toggleDropdown} />
+        <ul className={`main-nav ${mobileLinkState}`} onClick={ handleMobileClick }>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/membership">Membership</Link>
+          </li>
+          <li>
+            <Link to="/awards-service">Awards and Service</Link>
+          </li>
+          <li>
+            <Link to="/resources">Resources</Link>
+          </li>
+          <li>Login</li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
 
 export default Navbar
