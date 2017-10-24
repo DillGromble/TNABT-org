d04f1fce77962390b7cdcfaea23c70e7a0dd4c78
Josh White <whitejosh42@gmail.com>
2017-10-24 10:25:35 -0500

    feat(mongo): basic db setup

--- server/api/apply.js
+++ server/api/apply.js
@@ -1,9 +1,19 @@
 const router = require('express').Router()
-
+const Member = require('../../db/Members')
 
 router.post('/membership', (req, res) => {
-  console.log(req.body)
-  res.sendStatus(200)
+  const testMember = Member(req.body)
+  // console.log('testMember:', testMember)
+
+  testMember.save(function (err) {
+    if (err) throw err
+    console.log('user created!')
+  })
+
+  Member.find({}, function (err, found) {
+    if (err) throw err
+    console.log(found)
+  })
 })
 
 module.exports = router
