af85bea0336ba901179cf0cf18084e8bc9539f5c
Josh White <whitejosh42@gmail.com>
2017-11-08 16:15:47 -0600

    refactored into two strategies

--- server/api/auth/facebook-auth.js
+++ server/api/auth/facebook-auth.js
@@ -5,18 +5,26 @@ const Member = require('../../../db/Members')
 
 let secret
 
+/*--------------------------------------------------------------------------*/
 // set secrets by trying dev file first, then defaulting to process.env
+/*--------------------------------------------------------------------------*/
 try {
   secret = require('../../../secrets')
   console.log('Facebook Auth info loaded from: secrets file')
 }
 catch (err) {
   console.log('Secrets not found... trying process.env')
-  if (process.env.FACEBOOK_APP_ID && process.env.FACEBOOK_SECRET && process.env.FACEBOOK_CALLBACK) {
+  if (
+    process.env.FACEBOOK_APP_ID &&
+    process.env.FACEBOOK_SECRET &&
+    process.env.FACEBOOK_CALLBACK_AUTH &&
+    process.env.FACEBOOK_CALLBACK_CONNECT
+  ) {
     secret = {
       FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
       FACEBOOK_SECRET: process.env.FACEBOOK_SECRET,
-      FACEBOOK_CALLBACK: process.env.FACEBOOK_CALLBACK
+      FACEBOOK_CALLBACK_AUTH: process.env.FACEBOOK_CALLBACK_AUTH,
+      FACEBOOK_CALLBACK_CONNECT: process.env.FACEBOOK_CALLBACK_CONNECT
     }
     console.log('Facebook Auth info loaded from : process.env')
   }
@@ -27,18 +35,19 @@ catch (err) {
 
 
 if (secret) {
-  const facebookConfig = {
+  /*--------------------------------------------------------------------------*/
+  // Strategy for auth via Facebook
+  /*--------------------------------------------------------------------------*/
+  const facebookConfigAUTH = {
     clientID: secret.FACEBOOK_APP_ID,
     clientSecret: secret.FACEBOOK_SECRET,
-    callbackURL: secret.FACEBOOK_CALLBACK,
+    callbackURL: secret.FACEBOOK_CALLBACK_AUTH,
     passReqToCallback: true
   }
 
-  const strategy = new FacebookStrategy(facebookConfig,
+  const facebookAuthStrategy = new FacebookStrategy(facebookConfigAUTH,
     (req, token, refreshToken, profile, done) => {
-
       process.nextTick(() => {
-
         // if user isn't logged in, look them up by facebook id
         if (!req.user) {
           Member.findOne({ 'facebook.id': profile.id }, (err, user) => {
@@ -47,25 +56,66 @@ if (secret) {
             else return done(null, false, { message: 'User not found' })
           })
         }
-        // user is logged in and is linking social auth to local account
         else {
-          const user = req.user
-          user.facebook.id = profile.id
-          user.facebook.token = token
-          user.facebook.name = `${profile.name.givenName} ${profile.name.familyName}`
-
-          user.save((err) => {
-            if (err) return console.error(err)
-            return done(null, user)
-          })
+          return done(null, false, { message: 'Cannot login twice' })
         }
       })
-    })
+    }
+  )
 
-  passport.use(strategy)
+  /*--------------------------------------------------------------------------*/
+  // Strategy for linking Facebook account to local auth
+  /*--------------------------------------------------------------------------*/
+  const facebookConfigCONNECT = {
+    clientID: secret.FACEBOOK_APP_ID,
+    clientSecret: secret.FACEBOOK_SECRET,
+    callbackURL: secret.FACEBOOK_CALLBACK_AUTH,
+    passReqToCallback: true
+  }
+
+  const facebookConnectStrategy = new FacebookStrategy(facebookConfigCONNECT,
+    (req, token, refreshToken, profile, done) => {
+      process.nextTick(() => {
+        const user = req.user
+        user.facebook.id = profile.id
+        user.facebook.token = token
+
+        user.save((err) => {
+          if (err) return console.error(err)
+          return done(null, user)
+        })
+      })
+    }
+  )
+
+  passport.use(facebookAuthStrategy)
+  passport.use(facebookConnectStrategy)
 }
 
+
+/*--------------------------------------------------------------------------*/
+// Routes
+/*--------------------------------------------------------------------------*/
 router
+
+  .get('/', passport.authenticate('facebook', { scope: 'email' }))
+
+  .get('/callback', passport.authenticate('facebook', {
+      successRedirect: '/',
+      failureRedirect: '/auth-failure'
+  }))
+
+
+  .get('/connect', passport.authorize('facebook', { scope: 'email' }))
+
+  .get('/connect/callback', passport.authorize('facebook', {
+    successRedirect: '/resources',
+    failureRedirect: '/'
+  }))
+
+
+module.exports = router
+
 
   // .get('/', (req, res, next) => {
   //   passport.authenticate('facebook', { scope: 'email' }, (err, user, info) => {
@@ -81,23 +131,3 @@ router
   //     })
   //   })(req, res, next)
   // })
-
-
-  .get('/', passport.authenticate('facebook', { scope: 'email' }))
-
-  .get('/callback', passport.authenticate('facebook', {
-      successRedirect: '/',
-      failureRedirect: '/auth-failure'
-    })
-  )
-
-  // auth/facebook/connect
-  .get('/connect', passport.authorize('facebook', { scope: 'email' }))
-
-  .get('/connect/callback', passport.authorize('facebook', {
-    successRedirect: '/resources',
-    failureRedirect: '/'
-  }))
-
-
-module.exports = router
