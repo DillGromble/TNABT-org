3941f8e511d5578ccd0daea3a2729ef16f036590
Josh White <whitejosh42@gmail.com>
2017-11-09 10:45:27 -0600

    feat(officers): added content for rest of officers

--- client/src/views/About/components/officers/index.js
+++ client/src/views/About/components/officers/index.js
@@ -5,23 +5,59 @@ import './officers.css'
 import Officer from './components/officer'
 
 import bob from './img/bob.png'
+import sadler from './img/ksadler.jpg'
+import alanfear from './img/alafear.jpg'
+import placeholder from './img/placeholder-headshot.png'
 
-const officerTest = {
-  name: 'Robert Pruitt',
-  office: 'Secretary',
-  school: 'Montgomery Bell Academy, Nashville',
-  classes: 'Biology I and AP Biology',
-  phone: '(615) 298-5514  ext: 326',
-  email: 'robert.pruitt@montgomerybell.edu',
-  image: bob
-}
+const officersArr = [
+  {
+    name: 'Brenda Royal',
+    office: 'President',
+    school: 'Central Magnet School',
+    classes: '---------',
+    phone: '(615) 904-6789',
+    email: 'royalb@rcschools.net',
+    image: placeholder
+  },
+  {
+    name: 'Kim Sadler',
+    office: 'Vice-President',
+    school: 'Middle Tennessee State University',
+    classes: '---------',
+    phone: '(615) 904-8283',
+    email: 'kim.sadler@mtsu.edu',
+    image: sadler
+  },
+  {
+    name: 'Alicja Lanfear',
+    office: 'Treasurer',
+    school: 'Middle Tennessee State University',
+    classes: '---------',
+    phone: '(615) 494-7626',
+    email: 'alicja.lanfear@mtsu.edu',
+    image: alanfear
+  },
+  {
+    name: 'Robert Pruitt',
+    office: 'Secretary',
+    school: 'Montgomery Bell Academy, Nashville',
+    classes: 'Biology I and AP Biology',
+    phone: '(615) 298-5514  ext: 326',
+    email: 'robert.pruitt@montgomerybell.edu',
+    image: bob
+  }
+]
 
 
 const Officers = () => (
   <section className="section-officers">
     <div className="row">
       <h2>Officers</h2>
-      <Officer {...officerTest} />
+      {
+        officersArr.map(officer => (
+          <Officer {...officer} key={officer.name} />
+        ))
+      }
     </div>
   </section>
 )
