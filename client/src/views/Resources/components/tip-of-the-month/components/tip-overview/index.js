1d247e6d289eff129b02127639259c440acee5b5
Josh White <whitejosh42@gmail.com>
2017-11-09 13:06:13 -0600

    feat(ToM): placeholder added and styled

--- client/src/views/Resources/components/tip-of-the-month/components/tip-overview/index.js
+++ client/src/views/Resources/components/tip-of-the-month/components/tip-overview/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 import './tip-overview.css'
+import placeholder from './tip-placeholder.png'
 
 
 const TipOverview = () => (
   <div className="tip-overview-container">
+    <h3 className="tip-header-placeholder">First tip of the month coming soon!</h3>
+    <img className="tip-img-placeholder" src={placeholder} alt="Tip of the Month coming soon!" />
   </div>
 )
 
